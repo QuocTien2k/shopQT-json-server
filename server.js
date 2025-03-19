65b69e05973e8a27ca1914b2a513c7a41ad39b00
QuocTien2k <hadesdqt@gmail.com>
2025-03-19 16:44:40 +0700

    update version json-server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const jsonServer = require("json-server");
-const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
 
 server.use(middlewares);
-server.use(router);
-server.listen(port, () => {
-  console.log(`✅ JSON Server is running on port ${port}`);
+server.use("/api", router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
 });
